fbdc51494db33025b234915d4cf86db424af56c3
Tony Li <tonyli.lives@gmail.com>
2014-04-22 20:49:14 -0700

    fixed last value returning undefined

--- heapqueue.js
+++ heapqueue.js
@@ -1,12 +1,12 @@
 /*
   This implementation is very loosely based off js-priority-queue
   by Adam Hooper from https://github.com/adamhooper/js-priority-queue
-  
+
   The js-priority-queue implementation seemed a teensy bit bloated
   with its require.js dependency and multiple storage strategies
-  when all but one were strongly discouraged. So here is a kind of 
+  when all but one were strongly discouraged. So here is a kind of
   condensed version of the functionality with only the features that
-  I particularly needed. 
+  I particularly needed.
 
   Using it is pretty simple, you just create an instance of HeapQueue
   while optionally specifying a comparator as the argument:
@@ -32,7 +32,7 @@
   The push method returns the new number of elements of the queue.
 
   You can push anything you'd like onto the queue, so long as your
-  comparator function is capable of handling it. The default 
+  comparator function is capable of handling it. The default
   comparator is really stupid so it won't be able to handle anything
   other than an number by default.
 
@@ -41,7 +41,7 @@
   heapq.push(-9999)
   heapq.peek() ==> -9999
 
-  The useful complement to to the push method is the pop method, 
+  The useful complement to to the push method is the pop method,
   which returns the smallest item and then removes it from the
   queue.
 
@@ -54,13 +54,13 @@
 
 */
 function HeapQueue(cmp){
-	this.cmp = (cmp || function(a, b){ return a - b });
+	this.cmp = (cmp || function(a, b){ return a - b; });
 	this.length = 0;
-	this.data = []
+	this.data = [];
 }
 HeapQueue.prototype.peek = function(){
-	return this.data[0]
-}
+	return this.data[0];
+};
 HeapQueue.prototype.push = function(value){
 	this.data.push(value);
 
@@ -70,14 +70,14 @@ HeapQueue.prototype.push = function(value){
 	while(pos > 0){
 		parent = (pos - 1) >>> 1;
 		if(this.cmp(this.data[pos], this.data[parent]) < 0){
-			x = this.data[parent]
+			x = this.data[parent];
 			this.data[parent] = this.data[pos];
 			this.data[pos] = x;
 			pos = parent;
 		}else break;
 	}
 	return ++this.length;
-}
+};
 HeapQueue.prototype.pop = function(){
 	var last_val = this.data.pop(),
 		ret = this.data[0];
@@ -93,12 +93,14 @@ HeapQueue.prototype.pop = function(){
 			if(left <= last && this.cmp(this.data[left], this.data[minIndex]) < 0) minIndex = left;
 			if(right <= last && this.cmp(this.data[right], this.data[minIndex]) < 0) minIndex = right;
 			if(minIndex !== pos){
-				x = this.data[minIndex]
-				this.data[minIndex] = this.data[pos]
+				x = this.data[minIndex];
+				this.data[minIndex] = this.data[pos];
 				this.data[pos] = x;
-				pos = minIndex
+				pos = minIndex;
 			}else break;
 		}
-	}
-	return ret
-}
+	} else {
+    ret = last_val;
+  }
+	return ret;
+};
